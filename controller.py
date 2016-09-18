293526ae926407f3e331706e96f5abddc09fc54d
gregmankes <gregmankes@gmail.com>
2016-09-18 14:03:35 -0400

    initialize repo, fix setup issues and add stop intent

---
+++ controller.py
@@ -0,0 +1,64 @@
+import logging
+from flask import Flask
+from flask_ask import Ask, statement,  session
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+import soco
+
+devices = []
+VOLUME_CHANGE = 10
+
+def setup(internal_counter=0):
+    devices = []
+    internal_counter += 1
+    for zone in soco.discover():
+        devices.append(zone)
+    if ((len(devices) < 1) and internal_counter < 5):
+        setup(internal_counter)
+
+@ask.intent('AMAZON.ResumeIntent')
+def resume():
+    for zone in devices:
+        if zone.is_coordinator:
+            zone.play()
+    return statement("Resuming Music")
+
+@ask.intent("PlayIntent")
+def play():
+    resume()
+    return statement("Playing Music")
+
+@ask.intent("AMAZON.PauseIntent")
+def pause():
+    for zone in devices:
+        if zone.is_coordinator:
+            zone.pause()
+    return statement("Pausing Music")
+
+@ask.intent("StopIntent")
+def stop():
+    pause()
+
+@ask.intent("AMAZON.NextIntent")
+def next():
+    for zone in devices:
+        if zone.is_coordinator:
+            zone.next()
+    return statement("")
+
+@ask.intent("VolumeUpIntent")
+def volume_up():
+    for zone in devices:
+        zone.volume += VOLUME_CHANGE
+    return statement("")
+
+@ask.intent("VolumeDownIntent")
+def volume_down():
+    for zone in devices:
+        zone.volume -= VOLUME_CHANGE
+    return statement("")
+
+if __name__ == '__main__':
+    setup()
+    app.run(debug=True)
